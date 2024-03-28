21a622daa714638cbdf52830a8b00401bef33299
root <reddyprashanth512@gmail.com>
2024-03-28 08:44:31 +0000

    solution

---
+++ src/components/Events/index.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import Header from "../Header";
+import "./index.css";
+
+class Events extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Events;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import "./index.css";
+
+const Header = () => (
+  <nav className="nav-container">
+    <img
+      src="https://res.cloudinary.com/dlsuy2qn2/image/upload/v1711614998/3_2x_1_qqhrv7.png"
+      alt="Logo"
+      className="image-logo"
+    />
+    <div>
+      <h1 className="heading">Hii</h1>
+    </div>
+    <img
+      src="https://res.cloudinary.com/dlsuy2qn2/image/upload/v1711615286/6_2x_1_oh1jul.png"
+      alt="Logo"
+      className="image-logo"
+    />
+  </nav>
+);
+export default Header;
